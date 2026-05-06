f24ed75ff5657c48b79b8c7ec672bece69f3ad0f
Aditya Sinha <ranjanaditya452@gmail.com>
2026-05-07 05:21:47 +0530

    changed base to a function instead of a class

--- core/database.py
+++ core/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy.orm import sessionmaker, declarative_base
 from core.config import DATABASE_URL
 
 DB_URL = DATABASE_URL
@@ -14,5 +14,4 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-class Base(DeclarativeBase):
-    pass
+Base = declarative_base()
