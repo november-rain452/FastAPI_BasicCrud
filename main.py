4d1c67eb157ef5a1302997c4da569ba910a139a3
Aditya Sinha <ranjanaditya452@gmail.com>
2026-05-10 22:40:02 +0530

    added global router

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
-
+from routes.router import api_router
 
 app = FastAPI()
+app.include_router(api_router)
--- routes/router.py
+++ routes/router.py
@@ -1,1 +1,6 @@
 from fastapi import APIRouter
+from routes import user_routes
+
+api_router = APIRouter()
+
+api_router.include_router(user_routes.router)
