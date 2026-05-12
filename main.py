1ad327510f43b7e7673f3b9ca527e355563e5855
Aditya Sinha <ranjanaditya452@gmail.com>
2026-05-12 10:14:04 +0530

    updated main.py to create database via orm and added init.py to load models on startup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from routes.router import api_router
+import models
+from core.database import engine,Base
 
 app = FastAPI()
 app.include_router(api_router)
+Base.metadata.create_all(bind=engine)
