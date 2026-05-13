c8bbfc202f8106cec403250c5aaa0eb4b2bf7936
Aditya Sinha <ranjanaditya452@gmail.com>
2026-05-13 06:14:18 +0530

    added every individual router to the app router

--- routes/router.py
+++ routes/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
-from routes import user_routes
+from routes import user_routes,product_routes,orders_routes
 
 api_router = APIRouter()
 
 api_router.include_router(user_routes.router)
+api_router.include_router(product_routes.router)
+api_router.include_router(orders_routes.router)
